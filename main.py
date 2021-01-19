f5e6e8fb902ddd5f18c54d1e228784db38664033
syth0le <chdan565@gamil.com>
2021-01-20 00:38:57 +0300

    for deploy

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
-from marshmallow_sqlalchemy import ModelSchema
-from marshmallow import fields
-from pprint import pprint
+from rest_api.models.recipes import RecipeSchema, Recipe
+from rest_api.models.categories import CategorySchema, Category
+from rest_api.models.summary import SummarySchema, Summary
+from rest_api.models.nutrition import NutritionSchema, Nutrition
+from rest_api.models.images import ImagesSchema, Images
+from rest_api.models.ingredients import IngredientsSchema, Ingredients
+from rest_api.models.steps import StepsSchema, Steps
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+from rest_api.utils.db_init import db
+
+
+# from pprint import pprint
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -14,254 +24,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test_db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
+# with app.app_context():
+#     db.create_all()
+# db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
-
-
-class Recipe(db.Model):
-    __tablename__ = 'recipes'
-
-    id = db.Column(db.Integer, unique=True, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(128), unique=True)
-    slug = db.Column(db.String(128), unique=True)
-    date = db.Column(db.String(128))
-    steps = db.relationship("Steps", backref="recipe", cascade="all, delete-orphan")
-    category = db.relationship("Category", backref="recipe", cascade="all, delete-orphan")
-    summary = db.relationship("Summary", backref="recipe", cascade="all, delete-orphan")
-    nutrition = db.relationship("Nutrition", backref="recipe", cascade="all, delete-orphan")
-    images = db.relationship("Images", backref="recipe", cascade="all, delete-orphan")
-    ingredients = db.relationship("Ingredients", backref="recipe", cascade="all, delete-orphan")
-
-    def __str__(self):
-        return self.title
-
-    def __repr__(self):
-        return '<Recipe %r>' % self.title
-
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-
-    def update(self, data):
-        for key, item in data:
-            setattr(self, key, item)
-        # self.modified_at = datetime.datetime.utcnow()
-        db.session.commit()
-
-    @classmethod
-    def get_all_recipes(cls):
-        return cls.query.all()
-
-    @classmethod
-    def find_recipe_by_title(cls, title):
-        return cls.query.filter_by(title=title).first()
-
-    """метод для вывода всего или что то там(крч метод all)чтобы сразу все собрать в json И не ебать себе мозг"""
-    #  но нужен ли он блять
-    # @classmethod
-    # def convertation_json(cls, title):
-    #     recipe_obj = cls.query.filter_by(title=title).first()
-
-
-class Category(db.Model):
-    __tablename__ = 'categories'
-
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
-    name = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return '<Category %r>' % self.name
-
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-
-class Summary(db.Model):
-    __tablename__ = 'summary'
-
-    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
-    name = db.Column(db.String(50), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    measure = db.Column(db.String(50), nullable=True)
-
-    def __repr__(self):
-        return '<Summary %r>' % self.name
-
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-
-class Nutrition(db.Model):
-    __tablename__ = 'nutrition'
-
-    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
-    name = db.Column(db.String(50), nullable=False)
-    quantity = db.Column(db.Float, nullable=False)
-    measure = db.Column(db.String(50), nullable=True)
-
-    def __repr__(self):
-        return '<Nutrition %r>' % self.name
-
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-
-class Images(db.Model):
-    __tablename__ = 'images'
-
-    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
-    name = db.Column(db.String(50), unique=True, nullable=False)
-    slug = db.Column(db.String(50), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<Images %r>' % self.name
-
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-
-class Ingredients(db.Model):
-    __tablename__ = 'ingredients'
-
-    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
-    name = db.Column(db.String(50), unique=True, nullable=False)
-    quantity = db.Column(db.Integer, unique=True, nullable=True)
-
-    def __repr__(self):
-        return '<Ingredients %r>' % self.name
-
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-
-class Steps(db.Model):
-    __tablename__ = 'steps'
-
-    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
-    name = db.Column(db.String(50), nullable=False)
-    text = db.Column(db.Text, nullable=False)
-
-    def __repr__(self):
-        return '<Steps %r>' % self.name
-
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-
-class CategorySchema(ModelSchema):
-    class Meta(ModelSchema.Meta):
-        model = Category
-        sqla_session = db.session
-
-    id = fields.Number(dump_only=True)
-    name = fields.String(required=True)
-    recipe_id = fields.Integer()
-
-
-class SummarySchema(ModelSchema):
-    class Meta(ModelSchema.Meta):
-        model = Summary
-        sqla_session = db.session
-
-    id = fields.Number(dump_only=True)
-    name = fields.String(required=True)
-    quantity = fields.Integer(required=True)
-    measure = fields.String(required=True)
-    recipe_id = fields.Integer()
-
-
-class NutritionSchema(ModelSchema):
-    class Meta(ModelSchema.Meta):
-        model = Nutrition
-        sqla_session = db.session
-
-    id = fields.Number(dump_only=True)
-    name = fields.String(required=True)
-    quantity = fields.Float(required=True)
-    measure = fields.String(required=True)
-    recipe_id = fields.Integer()
-
-
-class ImagesSchema(ModelSchema):
-    class Meta(ModelSchema.Meta):
-        model = Images
-        sqla_session = db.session
-
-    id = fields.Number(dump_only=True)
-    name = fields.String(required=True)
-    slug = fields.String(required=True)
-    recipe_id = fields.Integer()
-
-
-class IngredientsSchema(ModelSchema):
-    class Meta(ModelSchema.Meta):
-        model = Ingredients
-        sqla_session = db.session
-
-    id = fields.Number(dump_only=True)
-    name = fields.String(required=True)
-    quantity = fields.Integer(required=True)
-    recipe_id = fields.Integer()
-
-
-class StepsSchema(ModelSchema):
-    class Meta(ModelSchema.Meta):
-        model = Steps
-        sqla_session = db.session
-
-    id = fields.Number(dump_only=True)
-    name = fields.String(required=True)
-    text = fields.String(required=True)
-    recipe_id = fields.Integer()
-
-
-class RecipeSchema(ModelSchema):
-    class Meta(ModelSchema.Meta):
-        model = Recipe
-        sqla_session = db.session
-
-    id = fields.Number(dump_only=True)
-    title = fields.String(required=True)
-    slug = fields.String(required=True)
-    date = fields.String(required=True)
-    category = fields.Nested(CategorySchema, many=True, only=['name', 'id'])
-    summary = fields.Nested(SummarySchema, many=True, only=['name', 'quantity', 'measure', 'id'])
-    nutrition = fields.Nested(NutritionSchema, many=True, only=['name', 'quantity', 'measure', 'id'])
-    images = fields.Nested(ImagesSchema, many=True, only=['name', 'slug', 'id'])
-    ingredients = fields.Nested(IngredientsSchema, many=True, only=['name', 'quantity', 'id'])
-    steps = fields.Nested(StepsSchema, many=True, only=['name', 'text', 'id'])
-
-
-# recipe_schema = RecipeSchema()
-# recipes_schema = RecipeSchema(many=True)
 
 
 @app.route('/')
@@ -335,10 +103,5 @@ def update_recipe(title):
 @app.route('/recipes/<string:title>', methods=['DELETE'])
 def delete_recipe(title):
     current_recipe = Recipe.find_recipe_by_title(title)
-    db.session.delete(current_recipe)
-    db.session.commit()
+    current_recipe.delete()
     return f"{title} deleted"
-
-
-if __name__ == "__main__":
-    app.run()
---
+++ rest_api/models/recipes.py
@@ -0,0 +1,80 @@
+from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields
+from rest_api.utils.db_init import db
+from rest_api.models.categories import CategorySchema
+from rest_api.models.summary import SummarySchema
+from rest_api.models.nutrition import NutritionSchema
+from rest_api.models.images import ImagesSchema
+from rest_api.models.ingredients import IngredientsSchema
+from rest_api.models.steps import StepsSchema
+
+
+class Recipe(db.Model):
+    __tablename__ = 'recipes'
+
+    id = db.Column(db.Integer, unique=True, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(128), unique=True)
+    slug = db.Column(db.String(128), unique=True)
+    date = db.Column(db.String(128))
+    steps = db.relationship("Steps", backref="recipe", cascade="all, delete-orphan")
+    category = db.relationship("Category", backref="recipe", cascade="all, delete-orphan")
+    summary = db.relationship("Summary", backref="recipe", cascade="all, delete-orphan")
+    nutrition = db.relationship("Nutrition", backref="recipe", cascade="all, delete-orphan")
+    images = db.relationship("Images", backref="recipe", cascade="all, delete-orphan")
+    ingredients = db.relationship("Ingredients", backref="recipe", cascade="all, delete-orphan")
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return '<Recipe %r>' % self.title
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, data):
+        for key, item in data:
+            setattr(self, key, item)
+        # self.modified_at = datetime.datetime.utcnow()
+        db.session.commit()
+
+    @classmethod
+    def get_all_recipes(cls):
+        return cls.query.all()
+
+    @classmethod
+    def find_recipe_by_title(cls, title):
+        return cls.query.filter_by(title=title).first()
+
+    """метод для вывода всего или что то там(крч метод all)чтобы сразу все собрать в json И не ебать себе мозг"""
+    #  но нужен ли он блять
+    # @classmethod
+    # def convertation_json(cls, title):
+    #     recipe_obj = cls.query.filter_by(title=title).first()
+
+
+class RecipeSchema(ModelSchema):
+    class Meta(ModelSchema.Meta):
+        model = Recipe
+        sqla_session = db.session
+
+    id = fields.Number(dump_only=True)
+    title = fields.String(required=True)
+    slug = fields.String(required=True)
+    date = fields.String(required=True)
+    category = fields.Nested(CategorySchema, many=True, only=['name', 'id'])
+    summary = fields.Nested(SummarySchema, many=True, only=['name', 'quantity', 'measure', 'id'])
+    nutrition = fields.Nested(NutritionSchema, many=True, only=['name', 'quantity', 'measure', 'id'])
+    images = fields.Nested(ImagesSchema, many=True, only=['name', 'slug', 'id'])
+    ingredients = fields.Nested(IngredientsSchema, many=True, only=['name', 'quantity', 'id'])
+    steps = fields.Nested(StepsSchema, many=True, only=['name', 'text', 'id'])
